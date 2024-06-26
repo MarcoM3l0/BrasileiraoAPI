b945d8262398059f029c076252cbbd7ac9a51efb
Marco Melo <josemarcomelonascimento@gmail.com>
2024-06-26 00:16:29 -0300

    tratamento de erro

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import tabela2024 from './tabela.js';
 import express from 'express';
+import { modeloTime, modeloAtuaizacaoTime } from './validacao.js';
 
 const app = express();
 
@@ -22,8 +23,21 @@ app.get("/:sigla", (req, res) => {
 });
 
 app.put('/:sigla', (req, res) => {
-    const siglasinformada = req.params.sigla.toUpperCase();
-    const time = tabela2024.find((infoTime) => infoTime.sigla === siglasinformada);
+    const siglainformada = req.params.sigla.toUpperCase();
+    const time = tabela2024.find((infoTime) => infoTime.sigla === siglainformada);
+
+    if(!time){
+        res.status(404).send("Não há na séria A do brasileirão um time com a silga " + siglainformada);
+        return;
+    }
+
+    const resultadoValidacao = modeloAtuaizacaoTime.validate(req.body).error;
+
+    if(resultadoValidacao){
+        res.status(400).send(resultadoValidacao);
+        return;
+    }
+
     const campos = Object.keys(req.body);
 
     for(let campo of campos){
@@ -36,14 +50,28 @@ app.put('/:sigla', (req, res) => {
 
 app.post('/', (req, res) => {
     const timeNovo = req.body;
+
+    const resultadoValidacao = modeloTime.validate(timeNovo).error;
+
+    if(resultadoValidacao){
+        res.status(400).send(resultadoValidacao);
+        return;
+    }
+
     tabela2024.push(timeNovo);
 
-    res.status(200).send(timeNovo);
+    res.status(201).send(timeNovo);
 });
 
 app.delete('/:sigla', (req, res) =>{
-    const siglaInformada = req.params.sigla.toUpperCase();
-    const indiceTimeSelecionado = tabela2024.findIndex((infoTime) => infoTime.sigla === siglaInformada);
+    const siglainformada = req.params.sigla.toUpperCase();
+    const indiceTimeSelecionado = tabela2024.findIndex((infoTime) => infoTime.sigla === siglainformada);
+
+    if(indiceTimeSelecionado === -1){
+        res.status(404).send("Não há na séria A do brasileirão um time com a silga " + siglainformada);
+        return;
+    }
+
     const timeRemovido = tabela2024.splice(indiceTimeSelecionado, 1);
 
     res.status(200).send(timeRemovido);
