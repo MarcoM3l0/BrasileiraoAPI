26aaab23a1e6c32b1731881f2d22a56db254ed58
Marco Melo <josemarcomelonascimento@gmail.com>
2024-06-25 22:45:27 -0300

    Endpoint de atualização de dados

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
     res.status(200).send(tabela2024);
 });
@@ -18,5 +20,18 @@ app.get("/:sigla", (req, res) => {
 
     res.status(200).send(time);
 });
+
+app.put('/:sigla', (req, res) => {
+    const siglasinformada = req.params.sigla.toUpperCase();
+    const time = tabela2024.find(infoTime => infoTime.sigla === siglasinformada);
+    const campos = Object.keys(req.body);
+
+    for(let campo of campos){
+        time[campo] = req.body[campo]
+    }
+
+    res.status(200).send(time);
+
+})
 
 app.listen(300, () => console.log("servidor rodando com sucesso!"));
